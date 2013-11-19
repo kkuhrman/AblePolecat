e2f88945782fd302e89313fbeb0398cc9ff8270f
kkuhrman <webmaster@abledistributors.com>
2013-11-19 12:38:24 -0600

    Updates to local settings file

--- local.php
+++ local.php
@@ -12,64 +12,90 @@
 //
 // Location of local installation root directory.
 //
-// define('ABLE_POLECAT_LOCAL', __DIR__);
+// if (!defined('ABLE_POLECAT_LOCAL')) {
+  // define('ABLE_POLECAT_LOCAL', __DIR__);
+// }
 
 //
 // Define location of Able Polecat configuration directory.
 //
-// define('ABLE_POLECAT_CONF_PATH', ABLE_POLECAT_LOCAL . DIRECTORY_SEPARATOR . 'conf');
+// if (!defined('ABLE_POLECAT_CONF_PATH')) {
+//   define('ABLE_POLECAT_CONF_PATH', ABLE_POLECAT_LOCAL . DIRECTORY_SEPARATOR . 'conf');
+//}
 
 //
 // Define location of Able Polecat data directory.
 //
-// define('ABLE_POLECAT_DATA_PATH', ABLE_POLECAT_LOCAL . DIRECTORY_SEPARATOR . 'data');
+// if (!defined('ABLE_POLECAT_DATA_PATH')) {
+//   define('ABLE_POLECAT_DATA_PATH', ABLE_POLECAT_LOCAL . DIRECTORY_SEPARATOR . 'data');
+//}
 
 //
 // Define location of directory for Able Polecat development tools.
 //
-// define('ABLE_POLECAT_DEV_PATH', ABLE_POLECAT_LOCAL . DIRECTORY_SEPARATOR . 'dev');
+// if (!defined('ABLE_POLECAT_DEV_PATH')) {
+//   define('ABLE_POLECAT_DEV_PATH', ABLE_POLECAT_LOCAL . DIRECTORY_SEPARATOR . 'dev');
+//}
 
 //
 // Path to third party class libraries.
 //
-// define('ABLE_POLECAT_LIBS_PATH', ABLE_POLECAT_LOCAL . DIRECTORY_SEPARATOR . 'libs');
+// if (!defined('ABLE_POLECAT_LIBS_PATH')) {
+//   define('ABLE_POLECAT_LIBS_PATH', ABLE_POLECAT_LOCAL . DIRECTORY_SEPARATOR . 'libs');
+//}
 
 //
 // Log files directory.
 //
-// define('ABLE_POLECAT_LOGS_PATH', ABLE_POLECAT_LOCAL . DIRECTORY_SEPARATOR . 'logs');
+// if (!defined('ABLE_POLECAT_LOGS_PATH')) {
+//   define('ABLE_POLECAT_LOGS_PATH', ABLE_POLECAT_LOCAL . DIRECTORY_SEPARATOR . 'logs');
+//}
 
 //
 // Path to contributed modules directory.
 //
-// define('ABLE_POLECAT_MODS_PATH', ABLE_POLECAT_LOCAL . DIRECTORY_SEPARATOR . 'mods');
+// if (!defined('ABLE_POLECAT_MODS_PATH')) {
+//   define('ABLE_POLECAT_MODS_PATH', ABLE_POLECAT_LOCAL . DIRECTORY_SEPARATOR . 'mods');
+//}
 
 //
 // Define location of Able Polecat unit test and qa directory.
 //
-// define('ABLE_POLECAT_QA_PATH', ABLE_POLECAT_LOCAL . DIRECTORY_SEPARATOR . 'qa');
+// if (!defined('ABLE_POLECAT_QA_PATH')) {
+//   define('ABLE_POLECAT_QA_PATH', ABLE_POLECAT_LOCAL . DIRECTORY_SEPARATOR . 'qa');
+//}
 
 //
 // Define location of user directory.
 //
-// define('ABLE_POLECAT_USER_PATH', ABLE_POLECAT_LOCAL . DIRECTORY_SEPARATOR . 'user');
+// if (!defined('ABLE_POLECAT_USER_PATH')) {
+//   define('ABLE_POLECAT_USER_PATH', ABLE_POLECAT_LOCAL . DIRECTORY_SEPARATOR . 'user');
+//}
 
 //
 // Define location of user sites directory.
 //
-// define('ABLE_POLECAT_SITES_PATH', ABLE_POLECAT_USER_PATH . DIRECTORY_SEPARATOR . 'sites');
+// if (!defined('ABLE_POLECAT_SITES_PATH')) {
+//   define('ABLE_POLECAT_SITES_PATH', ABLE_POLECAT_USER_PATH . DIRECTORY_SEPARATOR . 'sites');
+//}
 
 //
 // Define location of user services directory.
 //
-// define('ABLE_POLECAT_SERVICES_PATH', ABLE_POLECAT_USER_PATH . DIRECTORY_SEPARATOR . 'services');
+// if (!defined('ABLE_POLECAT_SERVICES_PATH')) {
+//   define('ABLE_POLECAT_SERVICES_PATH', ABLE_POLECAT_USER_PATH . DIRECTORY_SEPARATOR . 'services');
+//}
 
 //
 // Define location of Able Polecat root directory (contains boot.php).
 //
-// define('ABLE_POLECAT_ROOT', __DIR__);
+// if (!defined('ABLE_POLECAT_ROOT')) {
+//   define('ABLE_POLECAT_ROOT', implode(DIRECTORY_SEPARATOR, array(dirname(__DIR__), 'public')));
+//}
 
 //
 // Define location of Able Polecat core class library.
 //
-// define('ABLE_POLECAT_PATH',  ABLE_POLECAT_ROOT . DIRECTORY_SEPARATOR . 'core');
+// if (!defined('ABLE_POLECAT_PATH')) {
+//   define('ABLE_POLECAT_PATH',  ABLE_POLECAT_ROOT . DIRECTORY_SEPARATOR . 'core');
+//}
