2ab9394145e1275d422280eb1cb6637f5dddd033
kkuhrman <kkuhrman@gmail.com>
2014-05-06 17:08:20 -0500

    Additional exception classes

---
+++ core/Exception/Message.php
@@ -0,0 +1,14 @@
+<?php 
+/**
+ * @file      polecat/Exception/Message.php
+ * @brief     Exceptions thrown by Able Polecat message (request/response) objects.
+ *
+ * @author    Karl Kuhrman
+ * @copyright [BDS II License] (https://github.com/kkuhrman/AblePolecat/blob/master/LICENSE.md)
+ * @version   0.5.0
+ */
+
+require_once(implode(DIRECTORY_SEPARATOR, array(ABLE_POLECAT_CORE, 'Exception.php')));
+
+class AblePolecat_Message_Exception extends AblePolecat_Exception {
+}
---
+++ core/Exception/QueryLanguage.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @file: Exception/QueryLanguage.php
+ * Exceptions thrown by query language objects.
+ */
+
+require_once(implode(DIRECTORY_SEPARATOR, array(ABLE_POLECAT_CORE, 'Exception.php')));
+
+class AblePolecat_QueryLanguage_Exception extends AblePolecat_Exception {
+}
