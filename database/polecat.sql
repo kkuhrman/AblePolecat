cb8fef81c6c2f6fe21b3423fc2b1a69c8f60b392
kkuhrman <kkuhrman@gmail.com>
2014-08-07 15:41:11 -0500

    Initial branch 0.6 changes to db resource, request tables

--- database/polecat.sql
+++ database/polecat.sql
@@ -3,14 +3,11 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jan 08, 2014 at 09:31 PM
+-- Generation Time: Aug 07, 2014 at 08:39 PM
 -- Server version: 5.5.8
 -- PHP Version: 5.3.5
 
-SET FOREIGN_KEY_CHECKS=0;
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT=0;
-START TRANSACTION;
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
@@ -43,16 +40,6 @@ CREATE TABLE IF NOT EXISTS `class` (
   KEY `classLibraryId` (`classLibraryId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Able Polecat class registry';
 
---
--- Dumping data for table `class`
---
-
-INSERT INTO `class` (`className`, `classId`, `classLibraryId`, `classScope`, `isRequired`, `classFullPath`, `classFactoryMethod`) VALUES
-('AblePolecat_Command_Target_FirePhp', 'ef12d460-73cf-11e3-981f-0800200c9a66', '3dc86b50-7228-11e3-981f-0800200c9a66', 'MOD', 'N', 'C:\\wamp\\www\\polecat\\private\\usr\\mods\\FirePhpLog\\Target.php', '__construct'),
-('AblePolecat_Log_FirePhp', '7474a61d-35d9-4d43-bb7c-848c8576ce4e', '3dc86b50-7228-11e3-981f-0800200c9a66', 'MOD', 'N', 'C:\\wamp\\www\\polecat\\private\\usr\\mods\\FirePhpLog\\Log.php', 'wakeup'),
-('AblePolecat_Service_Client_Apex', 'f5ec7a80-7159-11e2-bcfd-0800200c9a666', '60c3f480-7228-11e3-981f-0800200c9a66', 'MOD', 'N', 'C:\\wamp\\www\\polecat\\private\\usr\\mods\\Apex\\Client.php', 'wakeup'),
-('AblePolecat_Service_Test', '66c8d7e0-672b-11e3-949a-0800200c9a66', 'ABLEDIST.UT', 'EXT', 'N', 'C:\\wamp\\www\\polecat\\private\\usr\\abledist\\ut\\svc\\Service.php', 'wakeup');
-
 -- --------------------------------------------------------
 
 --
@@ -73,19 +60,6 @@ CREATE TABLE IF NOT EXISTS `classlib` (
   KEY `classLibraryType` (`classLibraryType`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 COMMENT='Third-party PHP class libraries used by modules';
 
---
--- Dumping data for table `classlib`
---
-
-INSERT INTO `classlib` (`classLibraryName`, `classLibraryId`, `classLibraryType`, `major`, `minor`, `revision`, `classLibraryDirectory`) VALUES
-('Able Distributors Unit Tests', 'ABLEDIST.UT', 'EXT', 0, 1, 0, 'C:\\wamp\\www\\polecat\\private\\usr\\abledist\\ut'),
-('Able Polecat Core Version 0.4.0-DEV', 'ABLE_POLECAT_CORE_0_4_0_DEV', 'CORE', 0, 4, 0, 'C:\\wamp\\www\\polecat\\public\\core'),
-('Apex', 'e63d28f0-7243-11e3-981f-0800200c9a66', 'API', 0, 0, 0, 'C:\\wamp\\www\\polecat\\private\\usr\\libs\\Apex'),
-('ApexClient', '60c3f480-7228-11e3-981f-0800200c9a66', 'MOD', 0, 1, 0, 'C:\\wamp\\www\\polecat\\private\\usr\\mods\\Apex'),
-('FirePHPCore', 'f25d6870-7243-11e3-981f-0800200c9a66', 'API', 0, 3, 2, 'C:\\wamp\\www\\polecat\\private\\usr\\libs\\FirePHPCore\\0.3.2'),
-('FirePHPLog', '3dc86b50-7228-11e3-981f-0800200c9a66', 'MOD', 0, 1, 0, 'C:\\wamp\\www\\polecat\\private\\usr\\mods\\FirePhpLog'),
-('ProvideXClient', '775711a0-7228-11e3-981f-0800200c9a66', 'MOD', 0, 1, 0, 'C:\\wamp\\www\\polecat\\private\\usr\\mods\\ProvideX');
-
 -- --------------------------------------------------------
 
 --
@@ -97,50 +71,6 @@ CREATE TABLE IF NOT EXISTS `interface` (
   `interfaceName` varchar(255) NOT NULL,
   PRIMARY KEY (`interfaceName`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `interface`
---
-
-INSERT INTO `interface` (`interfaceName`) VALUES
-('AblePolecat_AccessControl_AgentInterface'),
-('AblePolecat_AccessControl_ArticleInterface'),
-('AblePolecat_AccessControl_ResourceInterface'),
-('AblePolecat_AccessControl_Resource_LocaterInterface'),
-('AblePolecat_AccessControl_RoleInterface'),
-('AblePolecat_AccessControl_Role_UserInterface'),
-('AblePolecat_AccessControl_Role_User_AuthenticatedInterface'),
-('AblePolecat_AccessControl_Role_User_Authenticated_OAuth2Interface'),
-('AblePolecat_AccessControl_SubjectInterface'),
-('AblePolecat_ArgsListInterface'),
-('AblePolecat_CacheObjectInterface'),
-('AblePolecat_ConfInterface'),
-('AblePolecat_DatabaseInterface'),
-('AblePolecat_DataInterface'),
-('AblePolecat_Data_ExchangeMapInterface'),
-('AblePolecat_Data_TxfrInterface'),
-('AblePolecat_DynamicObjectInterface'),
-('AblePolecat_EnvironmentInterface'),
-('AblePolecat_LogInterface'),
-('AblePolecat_MessageInterface'),
-('AblePolecat_Message_RequestInterface'),
-('AblePolecat_Message_ResponseInterface'),
-('AblePolecat_MetaDataInterface'),
-('AblePolecat_ModeInterface'),
-('AblePolecat_OverloadableInterface'),
-('AblePolecat_QueryLanguageInterface'),
-('AblePolecat_QueryLanguage_ExpressionInterface'),
-('AblePolecat_QueryLanguage_Expression_BinaryInterface'),
-('AblePolecat_QueryLanguage_Expression_Binary_SqlInterface'),
-('AblePolecat_QueryLanguage_StatementInterface'),
-('AblePolecat_QueryLanguage_Statement_Sql_Interface'),
-('AblePolecat_Server_CheckInterface'),
-('AblePolecat_Service_ClientInterface'),
-('AblePolecat_Service_DtxInterface'),
-('AblePolecat_Service_InitiatorInterface'),
-('AblePolecat_Service_Interface'),
-('AblePolecat_SessionInterface'),
-('AblePolecat_TransactionInterface');
 
 -- --------------------------------------------------------
 
@@ -159,11 +89,6 @@ CREATE TABLE IF NOT EXISTS `locks` (
   KEY `createdbyid` (`createdbyid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `locks`
---
-
-
 -- --------------------------------------------------------
 
 --
@@ -179,22 +104,7 @@ CREATE TABLE IF NOT EXISTS `log` (
   `eventMessage` text NOT NULL,
   PRIMARY KEY (`eventId`),
   KEY `user_id` (`userId`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=41 ;
-
---
--- Dumping data for table `log`
---
-
-INSERT INTO `log` (`eventId`, `eventTime`, `userId`, `eventSeverity`, `eventMessage`) VALUES
-(32, '2014-01-03 15:46:26', 1, 'status', 'Error in Able Polecat. 8 Undefined index: defaultHeaders in C:wampwwwpolecatpubliccoreServer.php line 301'),
-(33, '2014-01-03 15:46:26', 1, 'status', 'Error in Able Polecat. 8 Undefined index: mimeType in C:wampwwwpolecatpubliccoreServer.php line 302'),
-(34, '2014-01-03 15:46:26', 1, 'status', 'Error in Able Polecat. 8 Undefined index: document in C:wampwwwpolecatpubliccoreServer.php line 304'),
-(35, '2014-01-06 16:48:37', 1, 'status', 'Error in Able Polecat. 8 Undefined variable: Subject in C:wampwwwpolecatpubliccoreServiceBus.php line 216'),
-(36, '2014-01-06 16:48:37', 1, 'status', 'Error in Able Polecat. 4096 Argument 1 passed to AblePolecat_Command_GetRegistry::invoke() must implement interface AblePolecat_AccessControl_SubjectInterface, null given, called in C:wampwwwpolecatpubliccoreServiceBus.php on line 216 and defined in C:wampwwwpolecatpubliccoreCommandGetRegistry.php line 41'),
-(37, '2014-01-06 16:52:37', 1, 'status', 'Error in Able Polecat. 8 Undefined variable: ClassInfo in C:wampwwwpolecatpubliccoreServiceBus.php line 222'),
-(38, '2014-01-06 17:19:16', 1, 'status', 'Error in Able Polecat. 8 Use of undefined constant ABLE_POLECAT_EXCEPTION_SVC_CLIENT_CONNECT_FAIL - assumed ''ABLE_POLECAT_EXCEPTION_SVC_CLIENT_CONNECT_FAIL'' in C:wampwwwpolecatprivateusrmodsApexClient.php line 66'),
-(39, '2014-01-07 10:18:57', 1, 'status', 'Error in Able Polecat. 2 array_flip() expects parameter 1 to be array, null given in C:wampwwwpolecatpubliccoreRegistryClass.php line 189'),
-(40, '2014-01-07 10:24:29', 1, 'status', 'Error in Able Polecat. 8 Undefined property: AblePolecat_Server::$Agent in C:wampwwwpolecatpubliccoreServer.php line 379');
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=20 ;
 
 -- --------------------------------------------------------
 
@@ -211,11 +121,6 @@ CREATE TABLE IF NOT EXISTS `outh2_login` (
   PRIMARY KEY (`login_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Stores information on attempts to login with OAuth 2.0.' AUTO_INCREMENT=1 ;
 
---
--- Dumping data for table `outh2_login`
---
-
-
 -- --------------------------------------------------------
 
 --
@@ -230,10 +135,55 @@ CREATE TABLE IF NOT EXISTS `outh2_token` (
   PRIMARY KEY (`session_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Stores OAuth 2.0 tokens based on session id.';
 
+-- --------------------------------------------------------
+
 --
--- Dumping data for table `outh2_token`
+-- Table structure for table `request`
 --
 
+DROP TABLE IF EXISTS `request`;
+CREATE TABLE IF NOT EXISTS `request` (
+  `requestId` bigint(20) NOT NULL AUTO_INCREMENT,
+  `requestTime` int(11) NOT NULL,
+  `remoteAddress` varchar(32) NOT NULL,
+  `remotePort` varchar(16) NOT NULL,
+  `userAgent` varchar(255) NOT NULL,
+  `requestMethod` varchar(16) NOT NULL,
+  `requestUri` varchar(255) NOT NULL,
+  PRIMARY KEY (`requestId`),
+  KEY `remoteAddress` (`remoteAddress`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `resource`
+--
+
+DROP TABLE IF EXISTS `resource`;
+CREATE TABLE IF NOT EXISTS `resource` (
+  `resourceName` varchar(255) NOT NULL,
+  `resourceId` char(36) DEFAULT NULL,
+  `className` varchar(255) NOT NULL,
+  PRIMARY KEY (`resourceName`),
+  UNIQUE KEY `resourceId` (`resourceId`),
+  KEY `className` (`className`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `response`
+--
+
+DROP TABLE IF EXISTS `response`;
+CREATE TABLE IF NOT EXISTS `response` (
+  `statusCode` char(3) NOT NULL,
+  `mimeType` varchar(255) NOT NULL DEFAULT '<?xml version=''1.0'' standalone=''yes''?>',
+  `defaultHeaders` text,
+  `body` text,
+  PRIMARY KEY (`statusCode`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -251,11 +201,6 @@ CREATE TABLE IF NOT EXISTS `role` (
   KEY `userId` (`userId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `role`
---
-
-
 -- --------------------------------------------------------
 
 --
@@ -265,18 +210,12 @@ CREATE TABLE IF NOT EXISTS `role` (
 DROP TABLE IF EXISTS `template`;
 CREATE TABLE IF NOT EXISTS `template` (
   `resourceId` varchar(255) NOT NULL,
+  `description` varchar(255) DEFAULT NULL,
   `mimeType` varchar(255) NOT NULL DEFAULT 'Content-type: text/xml; charset=utf-8',
   `defaultHeaders` text,
-  `document` text,
+  `body` text,
   PRIMARY KEY (`resourceId`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `template`
---
-
-INSERT INTO `template` (`resourceId`, `mimeType`, `defaultHeaders`, `document`) VALUES
-('52c72b2cae817', 'Content-type: text/html', 'a:0:{}', '<!DOCTYPE html>\r\n<html>\r\n  <head>\r\n    <title>Able Polecat | Status</title>\r\n  </head>\r\n  <body>\r\n    <div id="container" style="left:12px;width:520px; style="font-family: "Lucida Console", "Verdana", Sans-serif; border-style: solid; border-color: black;">\r\n    <div id="caption" style="position:relative; padding:4px; opacity:0.8;height:64px;background-color:grey; border-style: solid; border-color: white;">\r\n    <h2>Able Polecat &copy; Project</h2>\r\n    </div>\r\n    <div id="version" style="position:relative; padding:4px; background-color:palegoldenrod; border-style: solid; border-color: white;">\r\n      <h3>{POLECAT_VERSION}</h3>\r\n      <h5>{POLECAT_DBSTATE}</h5>\r\n    </div>\r\n    <div id="notice" style="position:relative; padding:4px; opacity:0.8;height:100px;background-color:grey; border-style: solid; border-color: white;">\r\n      <p>\r\n        <small>The Able Polecat Core is free software released ''as is'' under a BSD II license (see <a href="https://github.com/kkuhrman/AblePolecat/blob/master/LICENSE.md">license</a> for more detail.)</small>\r\n      </p>\r\n      <p>Copyright &copy; 2008-2013 <a href="http://www.abledistributors.com" target="new">Able Distributors Inc.</a> All rights reserved.</p>\r\n    </div>\r\n  </body>\r\n</html>');
 
 -- --------------------------------------------------------
 
@@ -293,13 +232,3 @@ CREATE TABLE IF NOT EXISTS `user` (
   PRIMARY KEY (`userId`),
   KEY `clientId` (`clientId`,`userName`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
-
---
--- Dumping data for table `user`
---
-
-INSERT INTO `user` (`userId`, `userAlias`, `clientId`, `userName`) VALUES
-(0, 'Anonymous', 'Able Polecat', 'Anonymous'),
-(1, 'System', 'Able Polecat', 'System');
-SET FOREIGN_KEY_CHECKS=1;
-COMMIT;
