de4fc3310e9f72ba938031d5cb965ce2cfa93430
kkuhrman <kkuhrman@gmail.com>
2014-10-23 15:20:55 -0500

    Exceptions thrown by DOM class

---
+++ core/Exception/Dom.php
@@ -0,0 +1,14 @@
+<?php 
+/**
+ * @file      polecat/core/Exception/Dom.php
+ * @brief     Exceptions thrown by Able Polecat AblePolecat_Dom.
+ *
+ * @author    Karl Kuhrman
+ * @copyright [BDS II License] (https://github.com/kkuhrman/AblePolecat/blob/master/LICENSE.md)
+ * @version   0.6.2
+ */
+
+require_once(implode(DIRECTORY_SEPARATOR, array(ABLE_POLECAT_CORE, 'Exception.php')));
+
+class AblePolecat_Dom_Exception extends AblePolecat_Exception {
+}
