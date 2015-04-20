6e1c00cbab27a550cd54cea425ab8739a2324403
kkuhrman <kkuhrman@gmail.com>
2015-04-20 10:44:16 -0500

    Update unit tests for AblePolecat_Data

--- usr/share/test/Data.php
+++ usr/share/test/Data.php
@@ -11,13 +11,54 @@
 require_once(implode(DIRECTORY_SEPARATOR, array(ABLE_POLECAT_CORE, 'UnitTest.php')));
 require_once(implode(DIRECTORY_SEPARATOR, array(ABLE_POLECAT_CORE, 'Data.php')));
 
-class AblePolecatTest_Data implements AblePolecat_UnitTestInterface {
+class AblePolecatTest_Data extends AblePolecat_UnitTest implements AblePolecat_UnitTestInterface {
   /**
    * Run all the tests in the class.
    *
    * @throw AblePolecat_UnitTest_Exception if any test fails.
    */
   public static function runTests() {
-    die('this is the end, my friend');
+    
+    self::test_getDataTypeName();
+    self::test_castPrimitiveType_pass();
+    AblePolecat_Debug::kill(self::getTestResults());
+  }
+  
+  public static function test_getDataTypeName() {
+    
+    try {
+      if (self::testMethod(
+        'AblePolecat_Data',
+        'getDataTypeName',
+        array(new AblePolecatTest_Data()),
+        'AblePolecatTest_Data'
+      )) {
+        self::setTestResult(__METHOD__, TRUE);
+      }
+    }
+    catch (AblePolecat_UnitTest_Exception $Exception) {
+      self::setTestResult(__METHOD__, FALSE, $Exception->getMessage());
+    }
+  }
+  
+  public static function test_castPrimitiveType_pass() {
+    try {
+      //
+      // Since expected result is not scalar must use AblePolecat_UnitTest_Result.
+      //
+      $expectedResult = new AblePolecat_UnitTest_Result();
+      $expectedResult->setDataTypeName('AblePolecat_Data_Primitive_Scalar_String');
+      if (self::testMethod(
+        'AblePolecat_Data',
+        'castPrimitiveType',
+        array('this is a string'),
+        $expectedResult
+      )) {
+        self::setTestResult(__METHOD__, TRUE);
+      }
+    }
+    catch (AblePolecat_UnitTest_Exception $Exception) {
+      self::setTestResult(__METHOD__, FALSE, $Exception->getMessage());
+    }
   }
  }
